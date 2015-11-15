de2371fd2bdf6043f4db81978094c94216d69c04
dscoonan <dsc995@gmail.com>
2015-11-14 19:25:04 -0600

    trying to merge added maps, working on place picker

--- RestaurantRoulette/app/src/main/java/com/cs371m/chuchu/restaurantroulette/CreateEvent.java
+++ RestaurantRoulette/app/src/main/java/com/cs371m/chuchu/restaurantroulette/CreateEvent.java
@@ -1,22 +1,34 @@
 package com.cs371m.chuchu.restaurantroulette;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
+import com.google.android.gms.common.GooglePlayServicesRepairableException;
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.ui.PlacePicker;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.gson.Gson;
 import com.parse.ParseException;
 import com.parse.ParseObject;
 import com.parse.SaveCallback;
 
+
+import org.json.JSONObject;
+
 public class CreateEvent extends AppCompatActivity {
 
     Toast toast;
+    int PLACE_PICKER_REQUEST = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +42,28 @@ public class CreateEvent extends AppCompatActivity {
         final EditText price = (EditText) findViewById(R.id.price);
 
         Button createEventButton = (Button) findViewById(R.id.createEventButton);
+        Button pickRestaurantButton = (Button) findViewById(R.id.PickRestaurantButton);
+
+        pickRestaurantButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try {
+
+                    PlacePicker.IntentBuilder builder = new PlacePicker.IntentBuilder();
+
+                    startActivityForResult(builder.build(CreateEvent.this), PLACE_PICKER_REQUEST);
+
+                } catch (GooglePlayServicesRepairableException e) {
+                    // ...
+                    Log.d("CreateEvent", e.getMessage());
+                } catch (GooglePlayServicesNotAvailableException e) {
+                    // ...
+                    Log.d("CreateEvent", e.getMessage());
+                }
+            }
+        });
+
+
 
         createEventButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -63,6 +97,24 @@ public class CreateEvent extends AppCompatActivity {
         }
         toast = Toast.makeText(getApplicationContext(), text, Toast.LENGTH_SHORT);
         toast.show();
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == PLACE_PICKER_REQUEST) {
+            if (resultCode == RESULT_OK) {
+                Place place = PlacePicker.getPlace(data, this);
+                Gson gson = new Gson();
+                String json = gson.toJson(place);
+//                Place result = gson.fromJson(json, Place.class);
+
+
+                String restaurantName = (String) place.getName();
+                LatLng restaurantLocation = place.getLatLng();
+
+                String toastMsg = String.format("Place: %s", place.getName());
+                Toast.makeText(this, toastMsg, Toast.LENGTH_LONG).show();
+            }
+        }
     }
 
 
--- RestaurantRoulette/app/src/main/java/com/cs371m/chuchu/restaurantroulette/MapsActivity.java
+++ RestaurantRoulette/app/src/main/java/com/cs371m/chuchu/restaurantroulette/MapsActivity.java
@@ -1,17 +1,43 @@
 package com.cs371m.chuchu.restaurantroulette;
 
+import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+import android.content.pm.PackageManager;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationManager;
+import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MapsActivity extends AppCompatActivity {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CircleOptions;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.parse.ParseUser;
+
+public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+    public LatLng myPosition;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
     }
 
     @Override
@@ -33,8 +59,77 @@ public class MapsActivity extends AppCompatActivity {
             return true;
         } else if (id == R.id.action_toggle_view) {
             finish();
+            return true;
+        } else if (id == R.id.action_account) {
+            if (ParseUser.getCurrentUser() == null) {
+                Intent intent = new Intent(this, Login.class);
+                startActivityForResult(intent, 1);
+            } else {
+                Intent intent = new Intent(this, Account.class);
+                startActivityForResult(intent, 1);
+            }
+            return true;
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        mMap.setMyLocationEnabled(true);
+        mMap.getUiSettings().setMyLocationButtonEnabled(true);
+
+        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+        // Creating a criteria object to retrieve provider
+        Criteria criteria = new Criteria();
+
+        // Getting the name of the best provider
+        String provider = LocationManager.NETWORK_PROVIDER;
+
+        Location location;
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            System.out.println("wrong permissions");
+
+            //    public void requestPermissions(@NonNull String[] permissions, int requestCode)
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for Activity#requestPermissions for more details.
+            location = null;
+        } else {
+            System.out.println("correct permissions");
+
+            location = locationManager.getLastKnownLocation(provider);
+        }
+
+//        System.out.println("=============position" + location.toString());
+
+        if (location != null) {
+            System.out.println("found a location");
+            double latitude = location.getLatitude();
+            double longitude = location.getLongitude();
+            myPosition = new LatLng(latitude, longitude);
+            mMap.addCircle(new CircleOptions().center(myPosition));
+            mMap.moveCamera(CameraUpdateFactory.newLatLng(myPosition));
+            mMap.moveCamera(CameraUpdateFactory.zoomTo(14));
+        }
+
+        // Add a marker in Sydney and move the camera
+//        LatLng sydney = new LatLng(-34, 151);
+//        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+//        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
 }
